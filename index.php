0c2c2c027a0d0678dc7ed78ddb0df04d78cb7da8
Hamadi88 <hamadi9konate@yahoo.fr>
2019-09-10 10:22:45 +0200

    modification htmlcss

--- index.php
+++ index.php
@@ -20,29 +20,104 @@
 
     <button class="accordion">File</button>
         <div class="file">
-                  <button>RC</button>
-                  <button>MC</button>
-                  <button>OM</button>
+                  <button class="rc">RC</button>
+                  <button class="mc">MC</button>
+                  <button class="om">OM</button>
         
         </div>
 
-    <button class="accordion">Placements RC / MOC / OM</button>
-        <div class="PlacRC">
-            <input type="checkbox" name="RC" id="RC">RC</input>
-            <input type="checkbox" name="MOC" id="MOC">MOC</input>
-            <input type="checkbox" name="OM" id="OM">OM</input>
+        <button class="accordion">Placements RC / MOC / OM</button>
+    <div class="PlacRC">
+        <div>
+            <input type="checkbox" id="cbx" class="cbx" style="display: none;">
+            <label for="cbx" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                    <span>MENS</span>
+            </label>
+
+               <input type="checkbox" id="cbx2" class="cbx" style="display: none;">
+                <label for="cbx2" class="check">
+                    <svg width="18px" height="18px" viewBox="0 0 18 18">
+                       <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                       <polyline points="1 9 7 14 15 4"></polyline>
+                    </svg>
+                      <span>DOM</span>
+                </label>
+
+            <input type="checkbox" id="cbx3" class="cbx" style="display: none;">
+            <label for="cbx3" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                    <span>FE</span>
+            </label>
+
+            <input type="checkbox" id="cbx4" class="cbx" style="display: none;">
+            <label for="cbx4" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                <span>ECA</span>
+            </label>
+
+            <input type="checkbox" id="cbx5" class="cbx" style="display:none;">
+            <label for="cbx5" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                <span>Appel RC Sans Placement</span>
+            </label>
+         
         </div>
+    </div>
+
 
 
     <button class="accordion">Placements Specifiques MOB / OM</button>
     <div class="PlacSpec">
-        
-            <input type="checkbox" name="SDC" id="SDC">SDC</input>
-            <input type="checkbox" name="ASU" id="ASU">ASU</input>
-            <input type="checkbox" name="OVERTE" id="OVERTE">OVERTE</input>
-            <input type="checkbox" name="GAZ" id="GAZ">GAZ</input>
+    <input type="checkbox" id="cbx6" class="cbx" style="display:none;">
+            <label for="cbx6" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                <span>SDC</span>
+            </label>
+
+        <input type="checkbox" id="cbx7" class="cbx" style="display:none;">
+            <label for="cbx7" class="check">
+                <svg width="18px" height="18px" viewBox="0 0 18 18">
+                    <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                    <polyline points="1 9 7 14 15 4"></polyline>
+                </svg>
+                <span>ASU</span>
+            </label>
+
+            <input type="checkbox" id="cbx8" class="cbx" style="display:none;">
+                <label for="cbx8" class="check">
+                    <svg width="18px" height="18px" viewBox="0 0 18 18">
+                         <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                         <polyline points="1 9 7 14 15 4"></polyline>
+                     </svg>
+                     <span>OVERTE</span>
+                </label>
+
+                <input type="checkbox" id="cbx9" class="cbx" style="display:none;">
+                    <label for="cbx9" class="check">
+                        <svg width="18px" height="18px" viewBox="0 0 18 18">
+                            <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+                            <polyline points="1 9 7 14 15 4"></polyline>
+                        </svg>
+                        <span>Gaz</span>
+                    </label>
     
-    </div>
+        </div>
 
 
     <button class="accordion">Typologie</button>
